ae4acefb7ac06d220f8d352e3c584866a46a3136
jaehyo <chojh0621@gmail.com>
2021-12-02 13:55:37 +0900

    Revert "modify analysispage to SwingMaster"
    This reverts commit 236afa1ef3f357df12584cdc5c85dbddb6081214, reversing
    changes made to d414d2b7bd628d33cc29f40f8767acf7741c4dfa.

--- SwingMaster/SwingMaster/settings.py
+++ SwingMaster/SwingMaster/settings.py
@@ -72,12 +72,8 @@ WSGI_APPLICATION = 'SwingMaster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SwingMaster',
-        'USER' : 'root',
-        'PASSWORD' : '0000',
-        'HOST' : '52.79.212.88',
-        'PORT' : '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
