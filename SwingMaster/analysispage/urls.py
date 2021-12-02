1f4b5417b11af8f9464ea336b495cbef78ffeda3
chojaehyo <69153087+chojaehyo@users.noreply.github.com>
2021-12-02 14:35:01 +0900

    Delete SwingMaster directory

--- SwingMaster/analysispage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'analysispage'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('file_upload', views.file_upload, name="file_upload"),
-]
